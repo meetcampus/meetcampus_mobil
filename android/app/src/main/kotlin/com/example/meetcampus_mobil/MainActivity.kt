121522c9ce7fee3cae7fdf2173c26de7851a8022
Hestys <hestu12@gmail.com>
2021-01-16 12:18:09 +0300

    start

---
+++ android/app/src/main/kotlin/com/example/meetcampus_mobil/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meetcampus_mobil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:meetcampus_mobil/screens/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'meetcampus',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
