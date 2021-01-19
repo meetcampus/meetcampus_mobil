cbd0b7132161bc0d3a6efad04041c750d39aba2a
Hestys <hestu12@gmail.com>
2021-01-19 22:43:53 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ void main() async {
       child: MyApp(),
       supportedLocales: [Locale('en', 'US'), Locale('tr', 'TR')],
       path: 'assets/language',
-      startLocale: Locale('tr', 'TR'),
       fallbackLocale: Locale('en', 'US'),
       saveLocale: true,
     ));
