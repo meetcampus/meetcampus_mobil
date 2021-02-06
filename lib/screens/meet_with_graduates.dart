f02fca0dd90aadc86498512089f90ae2a076b8be
Ferhat İltaş <iltasferhat@gmail.com>
2021-02-07 00:18:55 +0300

    Local Json Added

--- lib/screens/meet_with_graduates.dart
+++ lib/screens/meet_with_graduates.dart
@@ -7,6 +7,7 @@ import 'package:meetcampus_mobil/service/graduate_service.dart';
 import 'package:meetcampus_mobil/utilities/component.dart';
 import 'package:meetcampus_mobil/utilities/styles.dart';
 import 'package:http/http.dart' as http;
+
 //import 'package:easy_localization/easy_localization.dart';
 import 'package:responsive_flutter/responsive_flutter.dart';
 
@@ -17,6 +18,8 @@ class MeetWithGraduates extends KFDrawerContent {
 }
 
 class _MeetWithGraduatesState extends State<MeetWithGraduates> {
+List<GraduateModel> allPersonelList;
+
   Future<GraduateModel> getGraduatesData() async {
     final response = await http.get('http://cihatkaraboga.com/graduates.json');
     final mapJson = json.decode(response.body);
@@ -62,101 +65,118 @@ class _MeetWithGraduatesState extends State<MeetWithGraduates> {
                   height: 20,
                 ),
                 FutureBuilder(
-                  future: GraduateService().getGraduatesData(),
+                  future: readData(),
+                  // ignore: missing_return
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
-                      return Container(
-                        child: Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Center(
-                                  child: CircleAvatar(
-                                    maxRadius: ResponsiveFlutter.of(context)
-                                        .verticalScale(40),
-                                    backgroundColor: Colors.transparent,
-                                    backgroundImage:
-                                        NetworkImage(snapshot.data.avatarUrl),
-                                  ),
-                                ),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
+                      allPersonelList=snapshot.data;
+                      return ListView.builder(physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: allPersonelList.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+
+                                child: Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      Text(
-                                          snapshot.data.name +
-                                              ' ' +
-                                              snapshot.data.surname,
-                                          style: TextStyle(
-                                              fontSize:
-                                                  ResponsiveFlutter.of(context)
-                                                      .fontSize(3),
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold)),
-                                      Spacer(
+                                      Expanded(
                                         flex: 1,
+                                        child: Center(
+                                          child:  CircleAvatar(
+                                            maxRadius:
+                                                ResponsiveFlutter.of(context)
+                                                    .verticalScale(40),
+                                            backgroundColor: Colors.transparent,
+                                            backgroundImage: NetworkImage(
+                                                allPersonelList[index].avatarUrl.toString()),
+                                          ),
+                                        ),
                                       ),
-                                      Material(
-                                        clipBehavior: Clip.antiAlias,
-                                        borderRadius:
-                                            BorderRadius.circular(50.0),
-                                        color: Colors.white.withOpacity(0.0),
-                                        child: IconButton(
-                                            icon: Image.asset(
-                                                'assets/icons8_twitter_60px_2.png'),
-                                            onPressed: () {}),
-                                      ),
-                                      Material(
-                                        clipBehavior: Clip.antiAlias,
-                                        borderRadius:
-                                            BorderRadius.circular(50.0),
-                                        color: Colors.white.withOpacity(0.0),
-                                        child: IconButton(
-                                            icon: Image.asset(
-                                                'assets/icons8_linkedin_52px_1.png'),
-                                            onPressed: () {}),
-                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Text(
+                                                  allPersonelList[index].name +
+                                                      ' ' +
+                                                      allPersonelList[index].surname,
+                                                  style: TextStyle(
+                                                      fontSize:
+                                                          ResponsiveFlutter.of(
+                                                                  context)
+                                                              .fontSize(3),
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold)),
+                                              Spacer(
+                                                flex: 1,
+                                              ),
+                                              Material(
+                                                clipBehavior: Clip.antiAlias,
+                                                borderRadius:
+                                                    BorderRadius.circular(50.0),
+                                                color:
+                                                    Colors.white.withOpacity(0.0),
+                                                child: IconButton(
+                                                    icon: Image.asset(
+                                                        'assets/icons8_twitter_60px_2.png'),
+                                                    onPressed: () {}),
+                                              ),
+                                              Material(
+                                                clipBehavior: Clip.antiAlias,
+                                                borderRadius:
+                                                    BorderRadius.circular(50.0),
+                                                color:
+                                                    Colors.white.withOpacity(0.0),
+                                                child: IconButton(
+                                                    icon: Image.asset(
+                                                        'assets/icons8_linkedin_52px_1.png'),
+                                                    onPressed: () {}),
+                                              ),
+                                            ],
+                                          ),
+                                          Text(allPersonelList[index].title,
+                                              style: TextStyle(
+                                                  fontSize: ResponsiveFlutter.of(
+                                                          context)
+                                                      .fontSize(2),
+                                                  color: Colors.white)),
+                                          Text(
+                                              'Çalıştığı Kurum:' +
+                                                  allPersonelList[index].placeOfWork,
+                                              style: TextStyle(
+                                                  fontSize: ResponsiveFlutter.of(
+                                                          context)
+                                                      .fontSize(2),
+                                                  color: Colors.white)),
+                                        ],
+                                      )
                                     ],
                                   ),
-                                  Text(snapshot.data.title,
-                                      style: TextStyle(
-                                          fontSize:
-                                              ResponsiveFlutter.of(context)
-                                                  .fontSize(2),
-                                          color: Colors.white)),
-                                  Text(
-                                      'Çalıştığı Kurum:' +
-                                          snapshot.data.placeOfWork,
-                                      style: TextStyle(
-                                          fontSize:
-                                              ResponsiveFlutter.of(context)
-                                                  .fontSize(2),
-                                          color: Colors.white)),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                        width: dynamicW * 0.9,
-                        height: dynamicH * 0.3,
-                        decoration: BoxDecoration( 
-                          gradient: LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment(1, 0.0),
-                            colors: [
-                              Color(0xff14698E),
-                              Color(0xff35A9DB),
-                            ],
-                          ),
-                          color: backColorBlue,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      );
+                                ),
+                                width: dynamicW * 0.9,
+                                height: dynamicH * 0.3,
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment(1, 0.0),
+                                    colors: [
+                                      Color(0xff14698E),
+                                      Color(0xff35A9DB),
+                                    ],
+                                  ),
+                                  color: backColorBlue,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            );
+                          });
                     } else if (snapshot.hasError) {
                     } else {
                       return Center(
@@ -173,5 +193,18 @@ class _MeetWithGraduatesState extends State<MeetWithGraduates> {
             ),
           ),
         ));
+  }
+
+
+  Future<List> readData() async {
+
+    var localJson = await DefaultAssetBundle.of(context)
+        .loadString("assets/data/personelInformations.json");
+
+
+    List<GraduateModel> personelList =  (json.decode(localJson) as List).map((mapJson) => GraduateModel.fromJson(mapJson)).toList();
+
+
+    return personelList;
   }
 }
